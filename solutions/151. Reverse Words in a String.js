994a0c236e249e53f696b4930e1f79839b3221b0
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-03-17 23:06:26 +0300

    76 ms

---
+++ solutions/151. Reverse Words in a String.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+const reverseWords = str => str.trim().replace(/ +/g, ' ').split(' ').reverse().join(' ');
+​
+// Test cases
+// console.assert(reverseWords("the sky is blue") === "blue is sky the", 'test 1 failed');
+// console.assert(reverseWords("  hello world  ") === "world hello", 'test 2 failed');
+// console.assert(reverseWords("a good   example") === "example good a", 'test 3 failed');
+// console.assert(reverseWords("  Bob    Loves  Alice   ") === "Alice Loves Bob", 'test 4 failed');
+// console.assert(reverseWords("Alice does not even like bob") === "bob like even not does Alice", 'test 5 failed');
