202f8ab8145f6d75fa6bf62861ef5b70bd8a4412
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-03-23 20:28:27 +0300

    80 ms

---
+++ solutions/206. Reverse Linked List.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = head => {
+  if (!head) {
+    return null;
+  }
+​
+  let reversedList = new ListNode(head.val, null);
+​
+  let fast = head.next;
+​
+  while (fast !== null) {
+    reversedList = new ListNode(fast.val, reversedList);
+​
+    fast = fast.next;
+  }
+​
+  return reversedList;
+};
+​
+// Test cases
+// function ListNode(val, next) {
+//   this.val = (val===undefined ? 0 : val)
+//   this.next = (next===undefined ? null : next)
+// }
+//
+// const six = new ListNode(6);
+// const five = new ListNode(5);
+// const four = new ListNode(4, five);
+// const three = new ListNode(3, four);
+// const two = new ListNode(2, three);
+// const one = new ListNode(1, two);
+//
+// console.log(reverseList(one));
