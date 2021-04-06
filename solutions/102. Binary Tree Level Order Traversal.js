016d61892e36c5acec35917c2ef246dc5f6f10a7
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-04-07 02:34:18 +0300

    80 ms

---
+++ solutions/102. Binary Tree Level Order Traversal.js
@@ -0,0 +1,38 @@
+const levelOrder = (root) => {
+  if (!root) {
+    return []
+  }
+​
+  let currentGroup = [root];
+  const result = [];
+​
+  function doCheck(group, i) {
+    const nextGroup = [];
+​
+    result[i] = [];
+​
+    for (let node of group) {
+      if (node.val !== undefined) {
+        result[i].push(node.val);
+      }
+​
+      if (node.left) {
+        nextGroup.push(node.left);
+      }
+​
+      if (node.right) {
+        nextGroup.push(node.right);
+      }
+    }
+​
+    return nextGroup;
+  }
+​
+  let i = 0;
+  while (currentGroup.length) {
+    currentGroup = doCheck(currentGroup, i);
+    i++;
+  }
+​
+  return result;
+};
