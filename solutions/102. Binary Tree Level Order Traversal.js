4699c4c1d32f8d0ab715017d7468d7be36c25e13
Nadir <nadir.zhumabaev@yandex.ru>
2021-04-07 02:38:44 +0300

    feat: Added `levelOrderArray` solution

--- solutions/102. Binary Tree Level Order Traversal.js
+++ solutions/102. Binary Tree Level Order Traversal.js
@@ -1,3 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
 const levelOrder = (root) => {
   if (!root) {
     return []
@@ -36,3 +48,57 @@ const levelOrder = (root) => {
  
   return result;
 };
+
+// Test cases
+// function TreeNode(val, left, right) {
+//   this.val = (val===undefined ? 0 : val)
+//   this.left = (left===undefined ? null : left)
+//   this.right = (right===undefined ? null : right)
+// }
+
+// const six = new TreeNode(7);
+// const five = new TreeNode(15);
+// const two = new TreeNode(20, five, six);
+// const one = new TreeNode(9);
+// const zero = new TreeNode(3, one, two);
+
+// console.log(levelOrder(zero)); // [[3],[9,20],[15,7]]
+
+/**
+ * @param {number[]} root
+ * @return {number[][]}
+ */
+const levelOrderArray = (root) => {
+  if (!root) {
+    return [];
+  }
+
+  let level = 1;
+  let maxElements = level * 2;
+  let elements = 0;
+
+  const result = [[root[0]]];
+
+  for (let i = 1; i < root.length; i++) {
+    elements++;
+
+    if (elements > maxElements) {
+      level++;
+      maxElements = level * 2;
+      elements = 1;
+    }
+
+    if (root[i]) {
+      if (result[level] === undefined) {
+        result[level] = [root[i]]
+      } else {
+        result[level].push(root[i]);
+      }
+    }
+  }
+
+  return result;
+};
+
+// Test cases
+// console.log(levelOrderArray([3,9,20,null,null,15,7]));
