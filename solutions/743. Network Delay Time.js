21477ce4ef551eef80b68f831bcf5ab315947af3
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-05-06 00:55:51 +0300

    104 ms

---
+++ solutions/743. Network Delay Time.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const networkDelayTime = (times, n, k) => {
+  const timeToEachNode = Array.from({ length: n + 1 }, (_, i) => i === k ? 0 : Infinity);
+​
+  let hasTimeToEachNodeChanges = true;
+  for (let i = 1; i < n; i++) {
+    if (!hasTimeToEachNodeChanges) {
+      break;
+    }
+​
+    hasTimeToEachNodeChanges = false
+​
+    for (const [sourceNode, targetNode, signalTimeToTravel] of times) {
+      if (timeToEachNode[sourceNode] === Infinity) {
+        continue;
+      }
+​
+      if (timeToEachNode[targetNode] > timeToEachNode[sourceNode] + signalTimeToTravel) {
+        hasTimeToEachNodeChanges = true;
+​
+        timeToEachNode[targetNode] = timeToEachNode[sourceNode] + signalTimeToTravel;
+      }
+    }
+  }
+​
+  let maxTimeToTravel = 0;
+  for (let i = 1; i <= n; i++) {
+    maxTimeToTravel = Math.max(maxTimeToTravel, timeToEachNode[i]);
+  }
+​
+  return maxTimeToTravel !== Infinity
+    ? maxTimeToTravel
+    : -1;
+};
+​
