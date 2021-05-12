61576e52c5136706633443251d4ebf589fe447e8
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-05-12 21:38:01 +0300

    96 ms

---
+++ solutions/39. Combination Sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+const combinationSum = (candidates, target) => {
+  const result = [];
+​
+  const bt = (index, sum, elements) => {
+    if (sum === target) {
+      result.push(Array.from(elements));
+      return;
+    }
+​
+    if (sum < target) {
+      for (let candidateIndex = index; candidateIndex < candidates.length; candidateIndex++) {
+        elements.push(candidates[candidateIndex]);
+        bt(candidateIndex, sum + candidates[candidateIndex], elements);
+        elements.pop();
+      }
+    }
+    // else if (sum > target) {
+    //   // do nothing?
+    // }
+  };
+​
+  bt(0, 0, []);
+​
+  return result;
+};
+​
+// console.log(combinationSum([2,3,6,7], 7)); // [[2,2,3],[7]]
+​
