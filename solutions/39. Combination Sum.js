864f6c87490d9c6d0c01ad56caf3af61e9dd6373
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-05-12 21:39:01 +0300

    96 ms

--- solutions/39. Combination Sum.js
+++ solutions/39. Combination Sum.js
@@ -30,4 +30,5 @@ const combinationSum = (candidates, target) => {
 };
 ​
 // console.log(combinationSum([2,3,6,7], 7)); // [[2,2,3],[7]]
+// console.log(combinationSum([2,3,5], 8)); // [[2,2,2,2],[2,3,3],[3,5]]
 ​
