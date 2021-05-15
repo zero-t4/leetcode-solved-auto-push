34086413493fc901b0cc087fd598454aeaa3418e
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-05-15 17:30:59 +0300

    284 ms

--- solutions/329. Longest Increasing Path in a Matrix.js
+++ solutions/329. Longest Increasing Path in a Matrix.js
@@ -28,3 +28,50 @@ const longestIncreasingPath = (matrix) => {
 ​
     const topCell = matrix[top]?.[c] || 0;
     const bottomCell = matrix[bottom]?.[c] || 0;
+    const rightCell = matrix[r]?.[right] || 0;
+    const leftCell = matrix[r]?.[left] || 0;
+​
+    const currentCell = matrix[r][c];
+​
+    const topMax = topCell > currentCell
+      ? findLongestIncreasingPath(top, c)
+      : 0;
+​
+    const bottomMax = bottomCell > currentCell
+      ? findLongestIncreasingPath(bottom, c)
+      : 0;
+​
+    const rightMax = rightCell > currentCell
+      ? findLongestIncreasingPath(r, right)
+      : 0;
+​
+    const leftMax = leftCell > currentCell
+      ? findLongestIncreasingPath(r, left)
+      : 0;
+​
+​
+    max[`${r},${c}`] = Math.max(
+      topMax,
+      bottomMax,
+      rightMax,
+      leftMax,
+    ) + 1;
+​
+    return max[`${r},${c}`];
+  }
+​
+  for (let r = 0; r < rowLast; r++) {
+    for (let c = 0; c < colLast; c++) {
+      findLongestIncreasingPath(r, c);
+    }
+  }
+​
+  return Math.max(...Object.values(max));
+};
+​
+// console.log(longestIncreasingPath([
+//   [9,9,4],
+//   [6,6,8],
+//   [2,1,1],
+// ])); // [1, 2, 6, 9].
+​
