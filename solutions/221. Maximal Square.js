6acc7879f87bb36053887dac89ec9efa3514d4c9
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-05-20 20:16:29 +0300

    100 ms

--- solutions/221. Maximal Square.js
+++ solutions/221. Maximal Square.js
@@ -7,8 +7,12 @@ const maximalSquare = (matrix) => {
 ​
   for (let r = 0; r < matrix.length; r++) {
     for (let c = 0; c < matrix[0].length; c++) {
-      if (matrix[r][c] == 1) {
-        matrix[r][c] =  Math.min(matrix[r - 1]?.[c] ?? 0, matrix[r]?.[c - 1] ?? 0, matrix[r - 1]?.[c - 1] ?? 0) + 1;
+      if (matrix[r][c] === '1') {
+        matrix[r][c] =  Math.min(
+          matrix[r - 1]?.[c] ?? 0,
+          matrix[r]?.[c - 1] ?? 0,
+          matrix[r - 1]?.[c - 1] ?? 0,
+        ) + 1;
       }
 ​
       max = Math.max(matrix[r][c], max);
