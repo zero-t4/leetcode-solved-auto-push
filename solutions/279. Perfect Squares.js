aef4888b38d247e9b5f6d8ce5fef67a150219098
Nadir <7101905+zero-t4@users.noreply.github.com>
2021-05-26 01:58:51 +0300

    1676 ms

--- solutions/279. Perfect Squares.js
+++ solutions/279. Perfect Squares.js
@@ -1,76 +1,32 @@
-// 1. Create `perfectSquares` array from 1 to 9999
-const perfectSquares = [];
-​
-let i = 1;
-let square = 1;
-​
-do {
-  square = Math.pow(i, 2);
-  perfectSquares.push(square);
-  i++;
-} while (square < 1e4);
-​
-const binarySearchRange = (left, right, val) => {
-  const mid = Math.floor((right - left) / 2);
-​
-  if (perfectSquares[mid] === val) {
-    return [mid, mid];
-  }
-​
-  if (perfectSquares[mid] > val) {
-    if (perfectSquares[mid - 1] < val) {
-      return [left, mid - 1];
-    }
-​
-    return binarySearchRange(left, mid, val);
-  }
-​
-  if (perfectSquares[mid] < val) {
-    if (perfectSquares[mid + 1] > val) {
-      return [left, mid];
-    }
-​
-    return binarySearchRange(left, right + 1, val);
-  }
-};
-​
 /**
  * @param {number} n
  * @return {number}
  */
 const numSquares = (n) => {
-  // 2. Find nearest perfect square to `n`
-  const found = binarySearchRange(0, perfectSquares.length - 1, n);
+  const perfectSquares = Array.from({ length: n + 1 }, () => 0);
+  perfectSquares[1] = 1;
 ​
-  const [left, right] = found;
+  for (let i = 2; i < perfectSquares.length; i++) {
+    perfectSquares[i] = Infinity;
 ​
-  const smallestArr = [];
+    for (let j = 1; j < i; j++) {
+      const square = Math.pow(j, 2);
 ​
-  // 3. Iterate over zeroth perfect square to found one
-  for (let i = right; i >= left; i--) {
-    let leftN = n;
-    let smallest = 0;
-​
-    while (leftN - perfectSquares[i] >= 0) {
-      leftN -= perfectSquares[i];
-      smallest++;
+      if (i - square === 0) {
+        perfectSquares[i] = 1;
+        break;
+      } else if (i - square > 0) {
+        perfectSquares[i] = Math.min(
+          perfectSquares[i],
+          perfectSquares[i - square] + perfectSquares[square],
+        );
+      }
     }
-​
-    if (leftN === 0) {
-      smallestArr.push(smallest);
-      continue;
-    }
-​
-    smallestArr.push(smallest + numSquares(leftN));
   }
 ​
-  // 4. Find a min number of perfect square sums to create `n` num/
-  return Math.min(...smallestArr);
+  return perfectSquares[n]
 };
 ​
 // Test cases
-// console.log(numSquares(12)); // 3
-// console.log(numSquares(13)); // 2
-// console.log(numSquares(9)); // 1
-// console.log(numSquares(26)); // 2
+console.log(numSquares(12)); // 3
 ​
